3e3970d10dbba69cd93a368575db64fbd050c2b0
Vaishak Murali <33431826+vaishakmurali1997@users.noreply.github.com>
2017-12-13 01:08:17 +0530

    Update animals.kt

--- app/src/main/java/com/example/vaishakmurali/doopers/animals.kt
+++ app/src/main/java/com/example/vaishakmurali/doopers/animals.kt
@@ -5,4 +5,5 @@ import android.location.Location
 /**
  * Created by vaishakmurali on 23/11/17.
  */
+// Creating a data class for crearing animal like aliens 
 data class Animals(val name:String, val image:Int,val description:String, val points:Int, var latitude:Double, var longitude:Double,var IsReached:Boolean)
